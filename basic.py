c1ed7ed2de57e1bdfe837fa12dd7434c7ffddebb
patchar024-lang <patchar024@gmail.com>
2026-07-21 10:39:51 -0400

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,8 @@
+# printing a greating message
+print("hello, world")
+# printing my name 
+print("My name is Patrick")
+# print a fun fact about my self
+print("I love playing kickball")
+# preforming and doing a simple cauculation
+print("the result of 3 + 5 is:", 3 + 5)
