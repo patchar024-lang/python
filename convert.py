45f890a3f5bc85e53b4775cc0c7a1159d8164661
patchar024-lang <patchar024@gmail.com>
2026-07-24 10:44:08 -0400

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,22 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+print("Name :",name)
+print("Data type of Name is :",type(name))
+print("Age :",age)
+print("Data type of Age is :",type(age))
+print("Is Student :",is_student)
+print("Data type of Is Student is :",type(is_student))
+print("Weight :",weight)
+print("Data type of Weight is :",type(weight))
+print("\n After Type Casting.....")
+age = str = (age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int (weight)
+print(weight)
+print("Data Type of weight is", type(weight))
+
+
+ 
