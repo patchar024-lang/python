91a82cb18ea43dcd4811b034bb3c45959a863d9a
patchar024-lang <patchar024@gmail.com>
2026-07-27 15:47:46 -0400

    Add files via upload

---
+++ xxValue.py
@@ -0,0 +1,9 @@
+tree1 = 1
+tree2 = 2 
+tree3 = 3
+tree4 = 4 
+tree5 = 5
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+print("the sum of all 5 trees is: ", sum)
+average = sum/5
+print("the average of all of the trees is:  ", average)
