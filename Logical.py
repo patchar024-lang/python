24ff63f8151644d27024bdb57dfc499721aa1868
patchar024-lang <patchar024@gmail.com>
2026-07-29 16:12:20 -0400

    Add files via upload

---
+++ Logical.py
@@ -0,0 +1,17 @@
+a = 12
+b = 12
+c = 12
+print(not (a == b))
+print(not(b == c))
+a = "python"
+b = "coding"
+if not(a == b):
+    print((a, 'and', b, 'are different'))
+
+a = 4
+b = 5 
+if not(( a == 1) == b == 5:)):
+    print('Hello')
+
+
+
