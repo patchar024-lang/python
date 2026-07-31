30cfa47d869df67a5db2cf639cb1bb974ac200f9
patchar024-lang <patchar024@gmail.com>
2026-07-31 16:16:16 -0400

    Add files via upload

---
+++ After call age.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+
+# Check if the age falls within the range of 10 to 20
+if 10 <= age <= 20:
+    print("The age is between 10 and 20 years.")
+else:
+    print("The age is NOT between 10 and 20 years.")
