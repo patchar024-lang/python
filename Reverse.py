022cd9f7fd10af03395d09fd47352802b4a67c87
patchar024-lang <patchar024@gmail.com>
2026-08-01 15:58:22 -0400

    Add files via upload

---
+++ Reverse.py
@@ -0,0 +1,6 @@
+string = input("Please enter your own string: ")
+string2 = ('')
+for i in string:
+    string2 = i + string2
+print("\nThe Original string is =  ", string)
+print("The Reversed string is = ", string2)
