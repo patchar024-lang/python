44b84db94fe1cbdc89c5831ad055626265f7f1d5
patchar024-lang <patchar024@gmail.com>
2026-08-25 10:21:07 -0400

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,9 @@
+def weather_Condition():
+    print('the weather is pleasant in:', spring)
+    print('the weather is the same in:', autumn)
+
+spring = "autumn"
+
+autumn = spring
+
+weather_Condition()
