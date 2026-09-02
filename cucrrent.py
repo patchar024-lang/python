a1624272c18219f4d82fd4d062f2074711c06101
patchar024-lang <patchar024@gmail.com>
2026-09-02 10:55:20 -0400

    Add files via upload

---
+++ cucrrent.py
@@ -0,0 +1,7 @@
+from datetime import date , datetime 
+today = date.today()
+now = datetime.now()
+print("Current date is:", today)
+print("\nCurrent time is:", now)
+print("\nDate components:", today.day, today.month, today.year)
+
