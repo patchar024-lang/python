8bc77997e1968dfddefb251a46c3c5b948d70491
patchar024-lang <patchar024@gmail.com>
2026-09-04 15:01:21 -0400

    Add files via upload

---
+++ list.py
@@ -0,0 +1,9 @@
+empty_list = []
+print(empty_list)
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+triple = [1,2,3] * 3 
+print(triple)
+aList = [100, 200, 300, 400, 500]
+aList = aList[::-1]
+print(aList, "\n")
