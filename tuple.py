276e36a67a72ac626ac29215c97c791f8e5f22a3
patchar024-lang <patchar024@gmail.com>
2026-09-07 14:30:18 -0400

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,15 @@
+tuplex = ("tuple", False, 3.2, 1)
+print (tuplex)
+tuplex = (4, 6, 2, 1)
+print (tuplex)
+
+tuplex = tuplex + (9,)
+print (tuplex)
+tuplex = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1,)
+
+_slice = tuplex[3:5]
+print(_slice)
+
+_slice = tuplex[:6]
+print(_slice)
+
