77a8c3a0dfaab6cc17d30ab07afd09e6e68ff50d
patchar024-lang <patchar024@gmail.com>
2026-09-15 16:36:36 -0400

    Add files via upload

---
+++ country codes.py
@@ -0,0 +1,12 @@
+country_codes = { 'India': '0091',
+                   'Australia': '0025',
+                   'Usa': '001',
+                   'Nepal' : '00977',}
+print("Country code for India -")
+print(country_codes.get('India', 'Not Found'))
+
+
+
+print("Country code for Usa -")
+print(country_codes.get('Usa', 'Not Found'))
+                  
